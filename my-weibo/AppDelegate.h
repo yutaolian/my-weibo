de8064c2ebfe4667390aa4f7e795ca6e3a0e0bed
lyt <675376983@qq.com>
2015-03-10 11:24:25 +0800

    Initial Commit

---
+++ my-weibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  my-weibo
+//
+//  Created by lyt on 15/3/10.
+//  Copyright (c) 2015年 lyt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ my-weibo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  my-weibo
+//
+//  Created by lyt on 15/3/10.
+//  Copyright (c) 2015年 lyt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
